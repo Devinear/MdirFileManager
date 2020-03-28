424a5ebd05e88ca0df5e0db6c63b6eb141328ca0
Chahyung Lee <leech8757@gmail.com>
2020-03-28 23:21:59 +0900

    Create Empty Project

---
+++ app/src/main/java/com/example/mdirfilemanager/FileManager.kt
@@ -0,0 +1,12 @@
+package com.example.mdirfilemanager
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class FileManager : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
