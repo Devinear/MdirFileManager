31c6dd7b4102c90b49172eea05e0026f404d6aaa
Chahyung Lee <leech8757@gmail.com>
2020-03-31 22:18:13 +0900

    Permission 설정 - READ_EXTERNAL_STORAGE

--- app/src/main/java/com/example/mdirfilemanager/FileManager.kt
+++ app/src/main/java/com/example/mdirfilemanager/FileManager.kt
@@ -1,12 +1,45 @@
 package com.example.mdirfilemanager
 
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import java.util.jar.Manifest
 
 class FileManager : AppCompatActivity() {
 
+    companion object {
+        const val REQUEST_CODE = 1
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        checkPermission()
+    }
 
+    private fun checkPermission() {
+//        if(Build.VERSION.SDK_INT < Build.VERSION_CODES.M)
+//            return
+        val permissions = Array(1) {
+            "android.permission.READ_EXTERNAL_STORAGE"
+        }
+        if (ContextCompat.checkSelfPermission(this, permissions[0]) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, permissions, REQUEST_CODE)
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        if(requestCode == REQUEST_CODE) {
+            grantResults.forEach {
+                // 허용 미동의
+                if(it != PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(this, "권한 설정이 필요합니다.", Toast.LENGTH_SHORT).show()
+                    finish()
+                }
+            }
+        }
     }
 }
