c260b3f77b30c03880726929b0ecd960400376d4
Chahyung Lee <leech8757@gmail.com>
2020-04-15 23:29:36 +0900

    파일 확장자에 따른 파일 분류

--- app/src/main/java/com/example/mdirfilemanager/common/Enum.kt
+++ app/src/main/java/com/example/mdirfilemanager/common/Enum.kt
@@ -8,5 +8,16 @@ enum class FileType(@ColorRes val color : Int, val abbr: String /*abbreviation*/
     Dir(R.color.type_dir,   "[ SubDir ]", 2),
     Default(R.color.type_default, "", 3),
     None(R.color.type_white,"", 4),
-    Storage(R.color.type_storage, "", 5)
+    Storage(R.color.type_storage, "", 5),
+    Image(R.color.type_blue_1, "", 6),
+    Video(R.color.type_green_1, "", 7),
+    Audio(R.color.type_yellow, "", 8)
+}
+
+enum class ExtType {
+    Image,
+    Video,
+    Audio,
+    Document,
+    Default
 }
