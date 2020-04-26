6209db654fea9b9a1bcbda6ebfc5d6a545fa5171
Chahyung Lee <leech8757@gmail.com>
2020-04-26 21:21:59 +0900

    Mdir Activity 실행 동작 변경

--- app/src/main/java/com/example/projects/MainActivity.kt
+++ app/src/main/java/com/example/projects/MainActivity.kt
@@ -3,14 +3,20 @@ package com.example.projects
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import com.example.projects.mdir.FileManagerActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var btMdir : Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        startActivity(Intent(this, FileManagerActivity::class.java))
+        btMdir = findViewById(R.id.btn_mdir)
+        btMdir.setOnClickListener {
+            startActivity(Intent(this, FileManagerActivity::class.java))
+        }
     }
 }
