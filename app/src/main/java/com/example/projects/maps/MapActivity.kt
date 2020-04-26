1c55f5a92373f3c05c16ed0106b46ff337fd69a2
Chahyung Lee <leech8757@gmail.com>
2020-04-26 21:28:47 +0900

    Map Activity 임시 구조 추가 - layout

--- app/src/main/java/com/example/projects/maps/MapActivity.kt
+++ app/src/main/java/com/example/projects/maps/MapActivity.kt
@@ -2,11 +2,13 @@ package com.example.projects.maps
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.projects.R
 
 class MapActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.layout_map)
     }
 
 }
