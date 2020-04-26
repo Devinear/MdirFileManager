79ee8d8f1aa309d0c6855b613b216e704f272a66
Chahyung Lee <leech8757@gmail.com>
2020-04-26 21:26:00 +0900

    Map Activity 임시 구조 추가

--- app/src/main/java/com/example/projects/MainActivity.kt
+++ app/src/main/java/com/example/projects/MainActivity.kt
@@ -4,19 +4,21 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import com.example.projects.maps.MapActivity
 import com.example.projects.mdir.FileManagerActivity
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var btMdir : Button
+    private lateinit var btMap  : Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         btMdir = findViewById(R.id.btn_mdir)
-        btMdir.setOnClickListener {
-            startActivity(Intent(this, FileManagerActivity::class.java))
-        }
+        btMdir.setOnClickListener { startActivity(Intent(this, FileManagerActivity::class.java)) }
+        btMap = findViewById(R.id.btn_map)
+        btMap.setOnClickListener { startActivity(Intent(this, MapActivity::class.java)) }
     }
 }
---
+++ app/src/main/java/com/example/projects/maps/MapActivity.kt
@@ -0,0 +1,12 @@
+package com.example.projects.maps
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MapActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+}
