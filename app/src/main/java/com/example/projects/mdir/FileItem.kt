8d389b1784dbc97a6158605ad062f2ecbc0bcbe5
Devinear <leech8757@gmail.com>
2020-08-24 00:21:13 +0900

    DataBinding 적용 - Recycler View 임시

--- app/src/main/java/com/example/projects/mdir/FileItem.kt
+++ app/src/main/java/com/example/projects/mdir/FileItem.kt
@@ -2,4 +2,4 @@ package com.example.projects.mdir
 
 import com.example.projects.mdir.common.FileType
 
-data class FileItem(var name: String, var type: FileType, var ext: String, var byteSize: Long, var time: String)
+data class FileItem(var name: String, /*var type: String,*/ var type: FileType, var ext: String, var byteSize: Long, var time: String)
