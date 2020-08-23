122e41b48360b7765c8bfa7b7a48d8c2bc57a677
Devinear <leech8757@gmail.com>
2020-08-23 23:03:29 +0900

    DataBinding 적용 - Main Activity

--- app/src/main/java/com/example/projects/MainActivity.kt
+++ app/src/main/java/com/example/projects/MainActivity.kt
@@ -1,24 +1,34 @@
 package com.example.projects
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.example.projects.databinding.ActivityMainBinding
 import com.example.projects.maps.MapActivity
 import com.example.projects.mdir.FileManagerActivity
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var btMdir : Button
-    private lateinit var btMap  : Button
+//    private lateinit var btMdir : Button
+//    private lateinit var btMap  : Button
+//    private lateinit var binding : ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-        btMdir = findViewById(R.id.btn_mdir)
-        btMdir.setOnClickListener { startActivity(Intent(this, FileManagerActivity::class.java)) }
-        btMap = findViewById(R.id.btn_map)
-        btMap.setOnClickListener { startActivity(Intent(this, MapActivity::class.java)) }
+        val binding : ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.btnMdir.setOnClickListener {
+            startActivity(Intent(this, FileManagerActivity::class.java))
+        }
+        binding.btnMap.setOnClickListener {
+            startActivity(Intent(this, MapActivity::class.java))
+        }
+
+//        setContentView(R.layout.activity_main)
+//        btMdir = findViewById(R.id.btn_mdir)
+//        btMdir.setOnClickListener { startActivity(Intent(this, FileManagerActivity::class.java)) }
+//        btMap = findViewById(R.id.btn_map)
+//        btMap.setOnClickListener { startActivity(Intent(this, MapActivity::class.java)) }
     }
 }
