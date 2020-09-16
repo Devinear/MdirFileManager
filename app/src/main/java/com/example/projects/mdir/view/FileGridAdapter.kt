e122989c02c3593110a367db7904df7cbe4291c5
Devinear <leech8757@gmail.com>
2020-09-16 23:51:48 +0900

    File Grid Adapter 임시 추가

---
+++ app/src/main/java/com/example/projects/mdir/view/FileGridAdapter.kt
@@ -0,0 +1,24 @@
+package com.example.projects.mdir.view
+
+import android.content.Context
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.projects.databinding.ItemGridFileBinding
+
+class FileGridAdapter(private val context: Context) : RecyclerView.Adapter<FileGridAdapter.ViewHolder>() {
+
+    class ViewHolder(private val binding: ItemGridFileBinding) : RecyclerView.ViewHolder(binding.root)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+}
