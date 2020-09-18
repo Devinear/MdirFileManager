d8731a167e045d80d67789438182fabbeac2f7e1
Devinear <leech8757@gmail.com>
2020-09-18 17:13:35 +0900

    File Type Image 적용

--- app/src/main/java/com/example/projects/mdir/common/Enum.kt
+++ app/src/main/java/com/example/projects/mdir/common/Enum.kt
@@ -3,17 +3,17 @@ package com.example.projects.mdir.common
 import androidx.annotation.ColorRes
 import com.example.projects.R
 
-enum class FileType(@ColorRes val color : Int, val abbr: String /*abbreviation*/, val sort: Int) {
-    UpDir(R.color.type_dir, "[ Up-Dir ]", 1),
-    Dir(R.color.type_dir,   "[ SubDir ]", 2),
-    Default(R.color.type_default, "", 3),
-    None(R.color.type_white,"", 4),
-    Storage(R.color.type_storage, "", 5),
-    Image(R.color.type_blue_1, "", 6),
-    Video(R.color.type_green_1, "", 7),
-    Audio(R.color.type_green_2, "", 8),
-    Document(R.color.type_yellow, "", 9),
-    Zip(R.color.type_blue_2, "", 10)
+enum class FileType(@ColorRes val color : Int, val abbr: String /*abbreviation*/, val sort: Int, val drawableRes: Int) {
+    UpDir(R.color.type_dir, "[ Up-Dir ]", 1, R.drawable.outline_arrow_upward_white_48),
+    Dir(R.color.type_dir,   "[ SubDir ]", 2, R.drawable.outline_folder_white_48),
+    Default(R.color.type_default, "", 3, R.drawable.outline_insert_drive_file_white_48),
+    None(R.color.type_white,"", 4, R.drawable.outline_all_inclusive_white_48),
+    Storage(R.color.type_storage, "", 5, R.drawable.outline_sd_card_white_48),
+    Image(R.color.type_blue_1, "", 6, R.drawable.outline_insert_photo_white_48),
+    Video(R.color.type_green_1, "", 7, R.drawable.outline_slideshow_white_48),
+    Audio(R.color.type_green_2, "", 8, R.drawable.outline_audiotrack_white_48),
+    Document(R.color.type_yellow, "", 9, R.drawable.outline_text_snippet_white_48),
+    Zip(R.color.type_blue_2, "", 10, R.drawable.outline_save_white_48)
 }
 
 enum class ExtType {
