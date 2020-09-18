f3b16c2f4a3e863e0657797e4fd2e80e53a33610
Devinear <leech8757@gmail.com>
2020-09-18 15:28:11 +0900

    BindingAdapters 수정

--- app/src/main/java/com/example/projects/mdir/view/BindingAdapters.kt
+++ app/src/main/java/com/example/projects/mdir/view/BindingAdapters.kt
@@ -4,13 +4,15 @@ import androidx.databinding.BindingAdapter
 import androidx.databinding.ObservableArrayList
 import androidx.recyclerview.widget.RecyclerView
 import com.example.projects.mdir.data.FileItem
+import com.example.projects.mdir.view.base.BaseAdapter
 
 object BindingAdapters {
 
     @JvmStatic
     @BindingAdapter("bind:item")
     fun bindItem(recyclerView: RecyclerView, items: ObservableArrayList<FileItem>) {
-        val linearAdapter : FileLinearAdapter = recyclerView.adapter as FileLinearAdapter
-        linearAdapter.setFileItems(items)
+        (recyclerView.adapter as BaseAdapter).apply {
+            setFileItems(items)
+        }
     }
 }
