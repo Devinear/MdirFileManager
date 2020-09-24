695a4e1e9bbbb5b8d6b6c6d85c2126811f3b2d89
Devinear <leech8757@gmail.com>
2020-09-24 15:33:40 +0900

    File Type에 따른 Color 변경 및 추가

--- app/src/main/java/com/example/projects/mdir/common/Enum.kt
+++ app/src/main/java/com/example/projects/mdir/common/Enum.kt
@@ -9,11 +9,11 @@ enum class FileType(@ColorRes val color : Int, val abbr: String /*abbreviation*/
     Default(R.color.type_default, "", 3, R.drawable.outline_insert_drive_file_white_48),
     None(R.color.type_white,"", 4, R.drawable.outline_all_inclusive_white_48),
     Storage(R.color.type_storage, "", 5, R.drawable.outline_sd_card_white_48),
-    Image(R.color.type_blue_1, "", 6, R.drawable.outline_insert_photo_white_48),
-    Video(R.color.type_green_1, "", 7, R.drawable.outline_slideshow_white_48),
-    Audio(R.color.type_green_2, "", 8, R.drawable.outline_audiotrack_white_48),
-    Document(R.color.type_yellow, "", 9, R.drawable.outline_text_snippet_white_48),
-    Zip(R.color.type_blue_2, "", 10, R.drawable.outline_save_white_48)
+    Image(R.color.type_yellow_1, "", 6, R.drawable.outline_insert_photo_white_48),
+    Video(R.color.type_blue_2, "", 7, R.drawable.outline_slideshow_white_48),
+    Audio(R.color.type_blue_1, "", 8, R.drawable.outline_audiotrack_white_48),
+    Document(R.color.type_yellow_2, "", 9, R.drawable.outline_text_snippet_white_48),
+    Zip(R.color.type_green_1, "", 10, R.drawable.outline_save_white_48)
 }
 
 enum class ExtType {
