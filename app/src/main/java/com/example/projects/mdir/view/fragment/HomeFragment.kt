4d6b45420f4329e0050b4a2e879daa15791022dd
Devinear <leech8757@gmail.com>
2020-10-21 23:56:56 +0900

    작업내역 임시 반영

--- app/src/main/java/com/example/projects/mdir/view/fragment/HomeFragment.kt
+++ app/src/main/java/com/example/projects/mdir/view/fragment/HomeFragment.kt
@@ -1,7 +1,37 @@
 package com.example.projects.mdir.view.fragment
 
+import android.app.Activity
+import android.content.Context
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import com.example.projects.R
+import com.example.projects.databinding.LayoutHomeBinding
+import com.example.projects.mdir.FileManagerActivity
 
 class HomeFragment : Fragment() {
+
+    private lateinit var binding : LayoutHomeBinding
+    private lateinit var activity : Activity
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        activity = getActivity() as FileManagerActivity
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        binding = DataBindingUtil.setContentView(activity, R.layout.layout_home)
+
+        return binding.root
+    }
+
 
 }
