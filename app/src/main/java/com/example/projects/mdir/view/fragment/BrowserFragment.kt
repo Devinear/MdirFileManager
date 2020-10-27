5b9f1ededbd2c4aafa4f9ec74b126aed143882e1
Devinear <leech8757@gmail.com>
2020-10-27 23:59:13 +0900

    Browser 임시 구조

--- app/src/main/java/com/example/projects/mdir/view/fragment/BrowserFragment.kt
+++ app/src/main/java/com/example/projects/mdir/view/fragment/BrowserFragment.kt
@@ -1,7 +1,16 @@
 package com.example.projects.mdir.view.fragment
 
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.MutableLiveData
 
 class BrowserFragment : Fragment() {
 
+    // UI
+    val livePath = MutableLiveData<String>()
+    val liveDirs = MutableLiveData<Int>()
+    val liveFiles = MutableLiveData<Int>()
+    val liveImages = MutableLiveData<Int>()
+    val liveShow = MutableLiveData<String>()
+
+    fun onClickHome() = Unit
 }
