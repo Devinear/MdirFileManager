58cfdc823321d54bd9104ce301a078787afe0301
Devinear <leech8757@gmail.com>
2020-11-09 18:25:27 +0900

    File 임시 저장

--- app/src/main/java/com/example/projects/mdir/FileViewModel.kt
+++ app/src/main/java/com/example/projects/mdir/FileViewModel.kt
@@ -1,12 +1,37 @@
 package com.example.projects.mdir
 
 import android.app.Application
+import android.net.Uri
 import android.widget.Toast
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.example.projects.mdir.common.BrowserType
+import com.example.projects.mdir.data.FileItem
 
 class FileViewModel(val app: Application) : AndroidViewModel(app) {
 
+    private val _files = MutableLiveData<List<FileItem>>()
+    val files: LiveData<List<FileItem>>
+        get() = _files
 
+    private val _openDir = MutableLiveData<FileItem>()
+    val openDir: LiveData<FileItem>
+        get() = _openDir
+
+    private val _mode = MutableLiveData<BrowserType>()
+    val mode: LiveData<BrowserType>
+        get() = _mode
+
+//    private val _category = MutableLiveData<Category>()
+//    val category: LiveData<Category>
+//        get() = _category
+
+    private var rootUri: Uri = Uri.EMPTY
+
+    init {
+        _mode.value = BrowserType.Storage
+    }
 
     fun onClickStorage() {
         Toast.makeText(app, "STORAGE", Toast.LENGTH_SHORT).show()
--- app/src/main/java/com/example/projects/mdir/data/FileItem.kt
+++ app/src/main/java/com/example/projects/mdir/data/FileItem.kt
@@ -3,4 +3,12 @@ package com.example.projects.mdir.data
 import android.graphics.drawable.BitmapDrawable
 import com.example.projects.mdir.common.FileType
 
-data class FileItem(var name: String, var ext: String, var fullname: String = "", var path: String, var type: FileType, var byteSize: Long, var time: String, var drawable: BitmapDrawable? = null, var childCount: Int = 0)
+data class FileItem(var name: String,
+                    var ext: String,
+                    var fullname: String = "",
+                    var path: String,
+                    var type: FileType,
+                    var byteSize: Long,
+                    var time: String,
+                    var drawable: BitmapDrawable? = null,
+                    var childCount: Int = 0)
