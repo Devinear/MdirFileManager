49182cb520d91141a0374a0293be81f086ed8917
Devinear <leech8757@gmail.com>
2020-11-13 18:52:05 +0900

    Storage 동작 추가

--- app/src/main/java/com/example/projects/mdir/repository/AbsStorageRepository.kt
+++ app/src/main/java/com/example/projects/mdir/repository/AbsStorageRepository.kt
@@ -1,13 +1,24 @@
 package com.example.projects.mdir.repository
 
+import android.content.Context
+import androidx.documentfile.provider.DocumentFile
 import com.example.projects.mdir.data.FileItem
 
 abstract class AbsStorageRepository {
 
     internal var sortBy : SortBy = SortBy.Name
-
     internal var sortOrder : SortOrder = SortOrder.Ascending
+    internal var query : String = ""
 
     open fun loadDirectory(path: String) = mutableListOf<FileItem>()
+    open fun loadDirectory(tree: DocumentFile, context: Context) = mutableListOf<FileItem>()
 
+    fun sort(sortBy: SortBy, sortOrder: SortOrder) {
+        this.sortBy = sortBy
+        this.sortOrder = sortOrder
+    }
+
+    fun search(query: String) {
+        this.query = query
+    }
 }
--- app/src/main/java/com/example/projects/mdir/repository/ScopeStorageRepository.kt
+++ app/src/main/java/com/example/projects/mdir/repository/ScopeStorageRepository.kt
@@ -1,10 +1,16 @@
 package com.example.projects.mdir.repository
 
+import android.content.Context
+import androidx.documentfile.provider.DocumentFile
 import com.example.projects.mdir.data.FileItem
 
 class ScopeStorageRepository : AbsStorageRepository() {
 
     override fun loadDirectory(path: String): MutableList<FileItem> {
         return super.loadDirectory(path)
+    }
+
+    override fun loadDirectory(tree: DocumentFile, context: Context): MutableList<FileItem> {
+        return super.loadDirectory(tree, context)
     }
 }
