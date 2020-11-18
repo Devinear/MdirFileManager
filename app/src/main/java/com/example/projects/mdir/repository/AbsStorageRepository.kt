ef3a2c9dc7d8d68209608739c80d8646d5e9ca68
Devinear <leech8757@gmail.com>
2020-11-19 00:50:33 +0900

    StorageRepository - Sort 기본 동작 변경

--- app/src/main/java/com/example/projects/mdir/repository/AbsStorageRepository.kt
+++ app/src/main/java/com/example/projects/mdir/repository/AbsStorageRepository.kt
@@ -8,17 +8,18 @@ import java.io.File
 
 abstract class AbsStorageRepository {
 
-    internal var sortBy : SortBy = SortBy.Name
-    internal var sortOrder : SortOrder = SortOrder.Ascending
+    internal var sortPairFir : Pair<SortBy, SortOrder> = Pair(SortBy.Type, SortOrder.Ascending)
+    internal var sortPairSec : Pair<SortBy, SortOrder> = Pair(SortBy.Name, SortOrder.Ascending)
+
     internal var query : String = ""
 
     open fun loadDirectory(context: Context, file: File) = mutableListOf<FileItemEx>()
     open fun loadDirectory(context: Context, path: String) = mutableListOf<FileItemEx>()
     open fun loadDirectory(context: Context, tree: DocumentFile) = mutableListOf<FileItemEx>()
 
-    fun sort(sortBy: SortBy, sortOrder: SortOrder) {
-        this.sortBy = sortBy
-        this.sortOrder = sortOrder
+    fun sort(sortBy: SortBy, sortOrder: SortOrder, sortBySec: SortBy = SortBy.Name, sortOrderSec: SortOrder = SortOrder.Ascending) {
+        sortPairFir = Pair(sortBy, sortOrder)
+        sortPairSec = Pair(sortBySec, sortOrderSec)
     }
 
     fun search(query: String) {
