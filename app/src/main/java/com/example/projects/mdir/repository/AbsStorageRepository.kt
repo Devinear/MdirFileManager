39367dbb6cbd42044b68fac6ee120873c75c7f63
Devinear <leech8757@gmail.com>
2020-11-19 00:50:15 +0900

    StorageRepository - 기본 Item 타입 변경

--- app/src/main/java/com/example/projects/mdir/repository/AbsStorageRepository.kt
+++ app/src/main/java/com/example/projects/mdir/repository/AbsStorageRepository.kt
@@ -3,6 +3,7 @@ package com.example.projects.mdir.repository
 import android.content.Context
 import androidx.documentfile.provider.DocumentFile
 import com.example.projects.mdir.data.FileItem
+import com.example.projects.mdir.data.FileItemEx
 import java.io.File
 
 abstract class AbsStorageRepository {
@@ -11,9 +12,9 @@ abstract class AbsStorageRepository {
     internal var sortOrder : SortOrder = SortOrder.Ascending
     internal var query : String = ""
 
-    open fun loadDirectory(context: Context, file: File) = mutableListOf<FileItem>()
-    open fun loadDirectory(context: Context, path: String) = mutableListOf<FileItem>()
-    open fun loadDirectory(context: Context, tree: DocumentFile) = mutableListOf<FileItem>()
+    open fun loadDirectory(context: Context, file: File) = mutableListOf<FileItemEx>()
+    open fun loadDirectory(context: Context, path: String) = mutableListOf<FileItemEx>()
+    open fun loadDirectory(context: Context, tree: DocumentFile) = mutableListOf<FileItemEx>()
 
     fun sort(sortBy: SortBy, sortOrder: SortOrder) {
         this.sortBy = sortBy
--- app/src/main/java/com/example/projects/mdir/repository/LegacyStorageRepository.kt
+++ app/src/main/java/com/example/projects/mdir/repository/LegacyStorageRepository.kt
@@ -3,25 +3,24 @@ package com.example.projects.mdir.repository
 import android.content.Context
 import android.graphics.BitmapFactory
 import android.graphics.drawable.BitmapDrawable
-import com.example.projects.R
 import com.example.projects.mdir.common.ExtType
 import com.example.projects.mdir.common.FileType
 import com.example.projects.mdir.common.FileUtil
-import com.example.projects.mdir.data.FileItem
+import com.example.projects.mdir.data.FileItemEx
 import java.io.File
 import java.text.SimpleDateFormat
 import java.util.*
 
 class LegacyStorageRepository : AbsStorageRepository() {
 
-    override fun loadDirectory(context: Context, path: String): MutableList<FileItem> {
+    override fun loadDirectory(context: Context, path: String): MutableList<FileItemEx> {
         return loadDirectory(context, File(path))
     }
 
-    override fun loadDirectory(context: Context, file: File): MutableList<FileItem> {
+    override fun loadDirectory(context: Context, file: File): MutableList<FileItemEx> {
         if(!file.exists()) return mutableListOf()
 
-        val listLoadDirs = mutableListOf<FileItem>()
+        val listLoadDirs = mutableListOf<FileItemEx>()
 
         file.listFiles()?.forEach {
             var image : BitmapDrawable? = null
@@ -31,20 +30,20 @@ class LegacyStorageRepository : AbsStorageRepository() {
                     image = BitmapDrawable(context.resources,
                         BitmapFactory.decodeFile(subList[0].absolutePath, BitmapFactory.Options().apply { inSampleSize = 4 } ))
                 }
-                val item = FileItem(name = it.name, path = it.absolutePath, type = FileType.Dir,
-                    ext = "", byteSize = 0L, time = TIME.format(Date(it.lastModified())), drawable = image, childCount = it.list()?.size?:0)
-                listLoadDirs.add(item)
+//                val item = FileItem(name = it.name, path = it.absolutePath, type = FileType.Dir,
+//                    ext = "", byteSize = 0L, time = TIME.format(Date(it.lastModified())), drawable = image, childCount = it.list()?.size?:0)
+                listLoadDirs.add(FileItemEx(it.absolutePath).apply { drawable = image })
             }
             else {
                 val type = FileUtil.toFileType(FileUtil.getFileExtType(file.extension))
-                if(type == FileType.Image)
-                    image = BitmapDrawable(context.resources, BitmapFactory.decodeFile(file.absolutePath, BitmapFactory.Options().apply { inSampleSize = 4 } ))
-
-                val item = FileItem(name = FileUtil.getFileName(file.name), path = file.absolutePath, type = type,
-                    ext = FileUtil.getFileExt(file.name), byteSize = file.length(), time = TIME.format(Date(file.lastModified())), drawable = image, childCount = file.list()?.size?:0)
-                listLoadDirs.add(item)
+                image = if(type == FileType.Image) BitmapDrawable(context.resources, BitmapFactory.decodeFile(file.absolutePath, BitmapFactory.Options().apply { inSampleSize = 4 } )) else null
+//                val item = FileItem(name = FileUtil.getFileName(file.name), path = file.absolutePath, type = type,
+//                    ext = FileUtil.getFileExt(file.name), byteSize = file.length(), time = TIME.format(Date(file.lastModified())), drawable = image, childCount = file.list()?.size?:0)
+                listLoadDirs.add(FileItemEx(it.absolutePath).apply { drawable = image })
             }
         }
+        sort(listLoadDirs)
+
         return listLoadDirs
     }
 
