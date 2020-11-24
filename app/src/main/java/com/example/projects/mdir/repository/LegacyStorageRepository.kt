9de263d2f51a0e49711444e809ce71f137c1f321
Devinear <leech8757@gmail.com>
2020-11-24 23:46:45 +0900

    StorageRepository - 불필요 코드 제거

--- app/src/main/java/com/example/projects/mdir/repository/LegacyStorageRepository.kt
+++ app/src/main/java/com/example/projects/mdir/repository/LegacyStorageRepository.kt
@@ -30,15 +30,11 @@ class LegacyStorageRepository : AbsStorageRepository() {
                     image = BitmapDrawable(context.resources,
                         BitmapFactory.decodeFile(subList[0].absolutePath, BitmapFactory.Options().apply { inSampleSize = 4 } ))
                 }
-//                val item = FileItem(name = it.name, path = it.absolutePath, type = FileType.Dir,
-//                    ext = "", byteSize = 0L, time = TIME.format(Date(it.lastModified())), drawable = image, childCount = it.list()?.size?:0)
                 listLoadDirs.add(FileItemEx(it.absolutePath).apply { drawable = image })
             }
             else {
                 val type = FileUtil.toFileType(FileUtil.getFileExtType(file.extension))
                 image = if(type == FileType.Image) BitmapDrawable(context.resources, BitmapFactory.decodeFile(file.absolutePath, BitmapFactory.Options().apply { inSampleSize = 4 } )) else null
-//                val item = FileItem(name = FileUtil.getFileName(file.name), path = file.absolutePath, type = type,
-//                    ext = FileUtil.getFileExt(file.name), byteSize = file.length(), time = TIME.format(Date(file.lastModified())), drawable = image, childCount = file.list()?.size?:0)
                 listLoadDirs.add(FileItemEx(it.absolutePath).apply { drawable = image })
             }
         }
