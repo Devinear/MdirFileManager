3b3ef8ca676b5ffbe0fe6c5f6185d067ea26d0cc
Devinear <leech8757@gmail.com>
2020-12-02 23:32:55 +0900

    Browser Layout BindingAdapter 동작 추가

--- app/src/main/java/com/example/projects/mdir/view/BindingAdapters.kt
+++ app/src/main/java/com/example/projects/mdir/view/BindingAdapters.kt
@@ -3,6 +3,7 @@ package com.example.projects.mdir.view
 import androidx.databinding.BindingAdapter
 import androidx.databinding.ObservableArrayList
 import androidx.recyclerview.widget.RecyclerView
+import com.example.projects.mdir.FileViewModel
 import com.example.projects.mdir.data.FileItemEx
 import com.example.projects.mdir.view.base.BaseAdapter
 
@@ -11,6 +12,14 @@ object BindingAdapters {
     @JvmStatic
     @BindingAdapter("bind:item")
     fun bindItem(recyclerView: RecyclerView, items: ObservableArrayList<FileItemEx>) {
+        (recyclerView.adapter as BaseAdapter).apply {
+            setFileItems(items)
+        }
+    }
+
+    @JvmStatic
+    @BindingAdapter("items", "viewModel")
+    fun setItems(recyclerView: RecyclerView, items: ObservableArrayList<FileItemEx>, viewModel: FileViewModel) {
         (recyclerView.adapter as BaseAdapter).apply {
             setFileItems(items)
         }
