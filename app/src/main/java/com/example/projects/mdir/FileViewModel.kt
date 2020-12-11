0818169af63fc695eabaf0909cd53d188b5c40b0
Devinear <leech8757@gmail.com>
2020-12-11 14:31:43 +0900

    FileViewModel execute / share 동작 추가

--- app/src/main/java/com/example/projects/mdir/FileViewModel.kt
+++ app/src/main/java/com/example/projects/mdir/FileViewModel.kt
@@ -12,6 +12,7 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
 import com.example.projects.mdir.common.BrowserType
+import com.example.projects.mdir.common.ExtType
 import com.example.projects.mdir.common.FileType
 import com.example.projects.mdir.common.FileUtil
 import com.example.projects.mdir.data.FileItemEx
@@ -82,29 +83,43 @@ class FileViewModel(val app: Application) : AndroidViewModel(app) {
                 _files.postValue(repository.loadDirectory(app, rootUri.path?:FileUtil.LEGACY_ROOT))
             }
             // 일반 파일
-            else -> {
-                val intent = Intent().apply {
-                    action = Intent.ACTION_VIEW
-                    flags = Intent.FLAG_ACTIVITY_NEW_TASK // setFlags
-                    addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
-                    addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
-                    setDataAndType(
-                        FileProvider.getUriForFile(app, "${app.packageName}.provider", item),
-                        FileUtil.getMimeType(item.extension)
-                    )
-                }
-
-                // 해당 Intent를 처리할 수 있는 앱이 있는지 확인
-                val resolveInfo : List<ResolveInfo> = app.packageManager.queryIntentActivities(intent, 0)
-                when(resolveInfo.isNotEmpty()) {
-                    true -> app.startActivity(intent)
-                    else -> Toast.makeText(app, item.name, Toast.LENGTH_SHORT).show()
-                }
-            }
+            else -> { requestExecute(item) }
         }
     }
 
-    fun requestLongClickItem(item: FileItemEx) {
+    fun requestExecute(item: FileItemEx) {
+        val intent = Intent().apply {
+            action = Intent.ACTION_VIEW
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK // setFlags
+            addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
+            addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+            setDataAndType(
+                FileProvider.getUriForFile(app, "${app.packageName}.provider", item),
+                FileUtil.getMimeType(item.extension)
+            )
+        }
 
+        // 해당 Intent를 처리할 수 있는 앱이 있는지 확인
+        val resolveInfo : List<ResolveInfo> = app.packageManager.queryIntentActivities(intent, 0)
+        when(resolveInfo.isNotEmpty()) {
+            true -> app.startActivity(intent)
+            else -> Toast.makeText(app, item.name, Toast.LENGTH_SHORT).show()
+        }
     }
+
+    fun requestShare(item: FileItemEx) {
+        // java.lang.ClassCastException: java.net.URI cannot be cast to android.os.Parcelable 발생
+        app.startActivity(Intent.createChooser(Intent().apply {
+            action = Intent.ACTION_SEND
+            type = when (FileUtil.getFileExtType(item.extension)) {
+                ExtType.Image -> "image/*"
+                ExtType.Video -> "video/*"
+                ExtType.Audio -> "audio/*"
+                else -> "application/*"
+            }
+            putExtra(Intent.EXTRA_STREAM, File(item.absolutePath).toURI())
+        }, "Share: ${item.name}"))
+    }
+
+    fun requestLongClickItem(item: FileItemEx) = Unit
 }
