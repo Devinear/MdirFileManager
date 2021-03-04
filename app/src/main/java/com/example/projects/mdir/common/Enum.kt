a8f9798e9315c5562858be9e9efb83a2d3173356
chahyung-lee <chahyung-lee@godo.co.kr>
2021-03-04 23:19:33 +0900

    Enum FileType에 ExtType 미리 지정

--- app/src/main/java/com/example/projects/mdir/common/Enum.kt
+++ app/src/main/java/com/example/projects/mdir/common/Enum.kt
@@ -3,18 +3,18 @@ package com.example.projects.mdir.common
 import androidx.annotation.ColorRes
 import com.example.projects.R
 
-enum class FileType(@ColorRes val color : Int, val abbr: String /*abbreviation*/, val sort: Int, val drawableRes: Int) {
-    UpDir(R.color.type_dir, "[ Up-Dir ]", 1, R.drawable.outline_arrow_upward_white_48),
-    Dir(R.color.type_dir,   "[ SubDir ]", 2, R.drawable.outline_folder_white_48),
-    Default(R.color.type_default, "", 3, R.drawable.outline_insert_drive_file_white_48),
-    None(R.color.type_white,"", 4, R.drawable.outline_all_inclusive_white_48),
-    Storage(R.color.type_storage, "", 5, R.drawable.outline_sd_card_white_48),
-    Image(R.color.type_yellow_1, "", 6, R.drawable.outline_insert_photo_white_48),
-    Video(R.color.type_blue_2, "", 7, R.drawable.outline_slideshow_white_48),
-    Audio(R.color.colorBurlyWood, "", 8, R.drawable.outline_audiotrack_white_48),
-    Document(R.color.colorBlueViolet, "", 9, R.drawable.outline_description_white_48),
-    Zip(R.color.type_green_1, "", 10, R.drawable.outline_save_white_48),
-    APK(R.color.type_green_2, "", 11, R.drawable.outline_android_white_48)
+enum class FileType(@ColorRes val color : Int, val abbr: String /*abbreviation*/, val sort: Int, val drawableRes: Int, val extType: ExtType) {
+    UpDir(R.color.type_dir, "[ Up-Dir ]", 1, R.drawable.outline_arrow_upward_white_48, ExtType.Default),
+    Dir(R.color.type_dir,   "[ SubDir ]", 2, R.drawable.outline_folder_white_48, ExtType.Default),
+    Default(R.color.type_default, "", 3, R.drawable.outline_insert_drive_file_white_48, ExtType.Default),
+    None(R.color.type_white,"", 4, R.drawable.outline_all_inclusive_white_48, ExtType.Default),
+    Storage(R.color.type_storage, "", 5, R.drawable.outline_sd_card_white_48, ExtType.Default),
+    Image(R.color.type_yellow_1, "", 6, R.drawable.outline_insert_photo_white_48, ExtType.Image),
+    Video(R.color.type_blue_2, "", 7, R.drawable.outline_slideshow_white_48, ExtType.Video),
+    Audio(R.color.colorBurlyWood, "", 8, R.drawable.outline_audiotrack_white_48, ExtType.Audio),
+    Document(R.color.colorBlueViolet, "", 9, R.drawable.outline_description_white_48, ExtType.Document),
+    Zip(R.color.type_green_1, "", 10, R.drawable.outline_save_white_48, ExtType.Zip),
+    APK(R.color.type_green_2, "", 11, R.drawable.outline_android_white_48, ExtType.Apk)
 }
 
 enum class ExtType {
