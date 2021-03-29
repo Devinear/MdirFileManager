ae26bdbbbcf47ff63e2d4108575c45c2eb1543fc
chahyung-lee <chahyung-lee@godo.co.kr>
2021-03-29 22:17:31 +0900

    SortBy Favorite 활성화

--- app/src/main/java/com/example/projects/mdir/repository/SortBy.kt
+++ app/src/main/java/com/example/projects/mdir/repository/SortBy.kt
@@ -1,9 +1,9 @@
 package com.example.projects.mdir.repository
 
 sealed class SortBy {
-//    internal object Favorite : SortBy()
     internal object Name : SortBy()
     internal object Type : SortBy()
     internal object Size : SortBy()
     internal object Date : SortBy()
+    internal object Favorite : SortBy()
 }
