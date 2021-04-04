3d90608dbbccf324a3537a1c48d91fa53fdd343c
chahyung-lee <chahyung-lee@godo.co.kr>
2021-04-04 22:28:23 +0900

    SortDialog Layout 변경

--- app/src/main/java/com/example/projects/mdir/view/SortDialog.kt
+++ app/src/main/java/com/example/projects/mdir/view/SortDialog.kt
@@ -4,18 +4,24 @@ import android.app.Dialog
 import android.content.Context
 import android.os.Bundle
 import android.view.View
+import android.widget.ArrayAdapter
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelStore
+import androidx.lifecycle.ViewModelStoreOwner
 import com.example.projects.R
 import com.example.projects.databinding.LayoutSortBinding
+import com.example.projects.mdir.FileManagerActivity
 import com.example.projects.mdir.FileViewModel
+import com.example.projects.mdir.repository.SortBy
+import com.example.projects.mdir.repository.SortOrder
 
-class SortDialog(context: Context, val viewModel: FileViewModel) : Dialog(context) {
+class SortDialog(context: Context, val viewModel: FileViewModel) : Dialog(context), ViewModelStoreOwner {
 
-    private val binding : LayoutSortBinding = LayoutSortBinding.bind(
-        View.inflate(context, R.layout.layout_sort, null)).apply {
-        vm = viewModel
-    }
+    private val sortViewModel = SortViewModel()
+    private val binding : LayoutSortBinding
+        = LayoutSortBinding.bind(View.inflate(context, R.layout.layout_sort, null))
+        .apply { vm = sortViewModel }
 
     init {
         setContentView(binding.root)
@@ -23,6 +29,11 @@ class SortDialog(context: Context, val viewModel: FileViewModel) : Dialog(contex
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding.spFirSort.adapter = SortAdapter(context, SortBy.values())
+        binding.spFirOrder.adapter = SortAdapter(context, SortOrder.values())
+
+        binding.spSecSort.adapter = SortAdapter(context, SortBy.values())
+        binding.spSecOrder.adapter = SortAdapter(context, SortOrder.values())
 
         observeViewModel()
     }
@@ -30,6 +41,16 @@ class SortDialog(context: Context, val viewModel: FileViewModel) : Dialog(contex
     override fun dismiss() {
         super.dismiss()
     }
+
+    fun onClickOk() {
+        dismiss()
+    }
+
+    fun onClickCancel() {
+        dismiss()
+    }
+
+    override fun getViewModelStore(): ViewModelStore = viewModelStore
 
     private fun observeViewModel() {
 
