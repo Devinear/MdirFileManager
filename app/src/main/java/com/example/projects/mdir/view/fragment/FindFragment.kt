08e10463736214fec40b3319f553448604adf086
Devinear <leech8757@gmail.com>
2021-04-22 17:45:20 +0900

    FindFragment 필터 숨김 동작 추가

--- app/src/main/java/com/example/projects/mdir/view/fragment/FindFragment.kt
+++ app/src/main/java/com/example/projects/mdir/view/fragment/FindFragment.kt
@@ -1,25 +1,22 @@
 package com.example.projects.mdir.view.fragment
 
-import android.app.Activity
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import com.example.projects.R
+import com.example.projects.R.drawable.outline_arrow_downward_white_24
+import com.example.projects.R.drawable.outline_arrow_upward_white_24
 import com.example.projects.databinding.LayoutFindBinding
 import com.example.projects.mdir.common.FragmentType
 import com.example.projects.mdir.view.base.BaseFragment
 
 class FindFragment : BaseFragment() {
-    companion object {
-        val INSTANCE by lazy(LazyThreadSafetyMode.SYNCHRONIZED) {
-            FindFragment()
-        }
-    }
 
     private lateinit var binding : LayoutFindBinding
-    private lateinit var activity : Activity
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -32,11 +29,33 @@ class FindFragment : BaseFragment() {
             container,
             false
         )
+        binding.fragment = this
         return binding.root
     }
 
     override fun initUi() {
-        super.initUi()
         fragmentType = FragmentType.Find
+        super.initUi()
+    }
+
+    @SuppressLint("UseCompatLoadingForDrawables")
+    fun changeFilterView() {
+        Log.d(TAG, "changeFilterView")
+        val drawable = if(binding.laFilter.layout.visibility == View.VISIBLE) {
+            binding.laFilter.layout.visibility = View.GONE
+            requireContext().getDrawable(outline_arrow_upward_white_24)
+        }
+        else {
+            binding.laFilter.layout.visibility = View.VISIBLE
+            requireContext().getDrawable(outline_arrow_downward_white_24)
+        }
+        binding.btFilter.setCompoundDrawablesWithIntrinsicBounds(null, null, drawable, null)
+    }
+
+    companion object {
+        val INSTANCE by lazy(LazyThreadSafetyMode.SYNCHRONIZED) {
+            FindFragment()
+        }
+        private const val TAG = "[DE][FR] Find"
     }
 }
