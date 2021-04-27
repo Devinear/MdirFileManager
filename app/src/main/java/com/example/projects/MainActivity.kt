981941966b9847d8b9c50cff2844c4dbe41974c8
Devinear <leech8757@gmail.com>
2021-04-27 22:53:14 +0900

    코드 정리

--- app/src/main/java/com/example/projects/MainActivity.kt
+++ app/src/main/java/com/example/projects/MainActivity.kt
@@ -1,37 +1,12 @@
 package com.example.projects
 
-import android.content.Intent
 import android.os.Bundle
-import android.os.Handler
 import androidx.appcompat.app.AppCompatActivity
-import com.example.projects.mdir.FileManagerActivity
 
 class MainActivity : AppCompatActivity() {
-
-//    private lateinit var btMdir : Button
-//    private lateinit var btMap  : Button
-//    private lateinit var binding : ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-//        val binding : ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-//        binding.btnMdir.setOnClickListener {
-//            startActivity(Intent(this, FileManagerActivity::class.java))
-//        }
-//        binding.btnMap.setOnClickListener {
-//            startActivity(Intent(this, MapActivity::class.java))
-//        }
-
-        Handler().postDelayed({
-            startActivity(Intent(this, FileManagerActivity::class.java))
-            finish()
-        }, 100)
-
-//        setContentView(R.layout.activity_main)
-//        btMdir = findViewById(R.id.btn_mdir)
-//        btMdir.setOnClickListener { startActivity(Intent(this, FileManagerActivity::class.java)) }
-//        btMap = findViewById(R.id.btn_map)
-//        btMap.setOnClickListener { startActivity(Intent(this, MapActivity::class.java)) }
     }
 }
